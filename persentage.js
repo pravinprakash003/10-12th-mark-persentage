a104a24c7a0da97be30bef6c485719f7fd4e2b1a
pravinprakash003 <94243136+pravinprakash003@users.noreply.github.com>
2021-11-13 23:54:53 +0530

    Add files via upload

---
+++ persentage.js
@@ -0,0 +1,11 @@
+function mark(){
+    var value = document.getElementById("persentage").value;
+   var total = (value*100)/500;
+   var number = document.getElementById("num").innerHTML=total;
+}
+
+function per(){
+    var value = document.getElementById("persentage").value;
+   var total = (value*100)/600;
+   var number = document.getElementById("num").innerHTML=total;
+}
